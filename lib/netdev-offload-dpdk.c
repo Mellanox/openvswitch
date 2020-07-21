Mellanox/OVS

commit 29845d092fdc48053998827389da5e3528c224e1

netdev-offload-dpdk: Delayed context release
The IDs may be used for restoring context of the packet processing.
Packets in the datapath are not synched with the SW, so there might be a
chance where datapath packets arrive with mark/meta for restoring, but
the context for those IDs might already be deleted. To overcome it,
instead of immediately release the context, put it in a list, and
actually release the context in a later phase. Here it is in offloading
other flows, so it is the same thread and no locking is needed.

Issue: 2193935
Change-Id: I0b3fc0d54d7bff214036a537bfca1beb8403eb10
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/netdev-offload-dpdk.c
@@ -319,6 +319,63 @@ context_release(struct context_metadata *md, void *arg, uint32_t id,
     hmap_remove(&md->d2i_hmap, &data_cur->d2i_node);
     free(data_cur);
     md->id_free(arg, id);
+    VLOG_DBG_RL(&rl, "%s: md=%s, id=%d", __func__, md->name, id);
+}
+
+static struct ovs_list context_release_list =
+    OVS_LIST_INITIALIZER(&context_release_list);
+
+struct context_release_item {
+    struct ovs_list node;
+    long long int timestamp;
+    struct context_metadata *md;
+    void *arg;
+    uint32_t id;
+    struct context_data *data;
+};
+
+static void
+context_delayed_release(struct context_metadata *md, void *arg, uint32_t id,
+                        struct context_data *data)
+{
+    struct context_release_item *item = xzalloc(sizeof *item);
+
+    item->md = md;
+    item->arg = arg;
+    item->id = id;
+    item->data= data;
+    item->timestamp = time_msec();
+    ovs_list_push_back(&context_release_list, &item->node);
+    VLOG_DBG_RL(&rl, "%s: md=%s, id=%d, timestamp=%llu", __func__,
+                item->md->name, item->id, item->timestamp);
+}
+
+#define DELAYED_RELEASE_TIMEOUT_MS 250
+/* In ofproto/ofproto-dpif-rid.c, function recirc_run. Timeout for expired
+ * flows is 250 msec. Set this timeout the same.
+ */
+
+static void
+do_context_delayed_release(void)
+{
+    struct context_release_item *item;
+    struct ovs_list *list;
+    long long int now;
+
+    now = time_msec();
+    while (!ovs_list_is_empty(&context_release_list)) {
+        list = ovs_list_front(&context_release_list);
+        item = CONTAINER_OF(list, struct context_release_item, node);
+        if (now < item->timestamp + DELAYED_RELEASE_TIMEOUT_MS) {
+            break;
+        }
+        VLOG_DBG_RL(&rl, "%s: md=%s, id=%d, timestamp=%llu, now=%llu",
+                    __func__, item->md->name, item->id, item->timestamp, now);
+        if (item->data->refcnt == 0) {
+            context_release(item->md, item->arg, item->id, item->data);
+        }
+        ovs_list_remove(list);
+    }
 }
 
 static void
@@ -342,7 +399,7 @@ put_context_data_by_id(struct context_metadata *md, void *arg, uint32_t id)
                         data_cur->refcnt, data_cur->id);
             ds_destroy(&s);
             if (data_cur->refcnt == 0) {
-                context_release(md, arg, id, data_cur);
+                context_delayed_release(md, arg, id, data_cur);
             }
             return;
         }
@@ -3754,6 +3811,8 @@ netdev_offload_dpdk_flow_put(struct netdev *netdev, struct match *match,
 {
     struct ufid_to_rte_flow_data *rte_flow_data;
     int ret;
+
+    do_context_delayed_release();
 
     /*
      * If an old rte_flow exists, it means it's a flow modification.

Mellanox/openvswitch

commit ec459fa1581eaecf80d415d7da3ba5db62c70990

netdev-offload-dpdk: Delay release only for miss contextes
There are HW contextes that should not be delayed. The only contextes
that should be delayed are the ones used for recovery of miss packets
(flow/CT miss contextes). Delay release only for those contextes.

Issue: 2239936
Fixes: 29845d092fdc ("netdev-offload-dpdk: Delayed context release")
Change-Id: Ibf65bbfade703f7fec02df1220d4761384131d7b
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/netdev-offload-dpdk.c
@@ -210,6 +210,7 @@ struct context_metadata {
     uint32_t (*id_alloc)(void *arg);
     void (*id_free)(void *arg, uint32_t id);
     size_t data_size;
+    bool delayed_release;
 };
 
 struct context_data {
@@ -351,13 +352,23 @@ static void
 context_delayed_release(struct context_metadata *md, void *arg, uint32_t id,
                         struct context_data *data, bool associated)
 {
-    struct context_release_item *item = xzalloc(sizeof *item);
+    struct context_release_item tmp_item, *item;
+
+    if (md->delayed_release) {
+        item = xzalloc(sizeof *item);
+    } else {
+        item = &tmp_item;
+    }
 
     item->md = md;
     item->arg = arg;
     item->id = id;
     item->data = data;
     item->associated = associated;
+    if (!md->delayed_release) {
+        context_release(item);
+        return;
+    }
     item->timestamp = time_msec();
     data->pending_release = true;
     ovs_list_push_back(&context_release_list, &item->node);
@@ -867,6 +878,7 @@ static struct context_metadata ct_miss_ctx_md = {
     .id_alloc = ct_ctx_id_alloc,
     .id_free = ct_ctx_id_free,
     .data_size = sizeof(struct ct_miss_ctx),
+    .delayed_release = true,
 };
 
 static int
@@ -1027,6 +1039,7 @@ static struct context_metadata flow_miss_ctx_md = {
     .id_alloc = flow_miss_id_alloc,
     .id_free = flow_miss_id_free,
     .data_size = sizeof(struct flow_miss_ctx),
+    .delayed_release = true,
 };
 
 static int