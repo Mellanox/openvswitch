Mellanox/OVS

commit 18efc6d35930aad717367c0b925e230ae54966a4

dpctl: Add CT insertion/deletion rates statistics
Usage: ovs-appctl dpctl/get-offload-stats -m
Dumps the multiple insertion rate points.

Issue: 2232395
Change-Id: I328da02192c63182f07c5eebcfce2005910f6b8b
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -2832,6 +2832,7 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
 {
     struct dp_offload_item *offload_item;
     struct ovs_list *list;
+    int ts_index = 0;
     const char *op;
     int ret;
 
@@ -2916,6 +2917,11 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
                      dp->offload_stats.ct_connections--;
                  }
                  break;
+            }
+            if ((dp->offload_stats.ct_connections &
+                 CT_CONN_TS_INTERVAL_MASK) == 0) {
+                ts_index &= CT_CONN_TS_MASK;
+                dp->offload_stats.timestamps[ts_index++] = time_msec();
             }
             if (ct_offload[CT_DIR_INIT].op == DP_NETDEV_FLOW_OFFLOAD_OP_ADD &&
                 rets[CT_DIR_INIT] && rets[CT_DIR_REP]) {
@@ -4205,9 +4211,12 @@ static int
 dpif_netdev_get_offload_stats(struct dpif *dpif, struct dpif_offload_stats *offload_stats)
 {
     struct dp_netdev *dp = get_dp_netdev(dpif);
+
     offload_stats->ct_connections = dp->offload_stats.ct_connections;
     offload_stats->queue_enqueues = dp_flow_offload.enqueues;
     offload_stats->queue_dequeues = dp_flow_offload.dequeues;
+    memcpy(&offload_stats->timestamps, &dp->offload_stats.timestamps,
+           sizeof offload_stats->timestamps);
 
     return 0;
 }

lib/dpctl.c
@@ -1114,12 +1114,38 @@ dpctl_get_offload_stats(int argc, const char *argv[],
 
         error = dpif_get_offload_stats(dpif, &offload_stats);
         if (!error) {
+            struct ds s = DS_EMPTY_INITIALIZER;
+            int i;
+
+            if (dpctl_p->verbosity) {
+                ds_put_format(&s, "CT insertion/deletion rates (%u resolution,"
+                              " K connections/sec):\n", CT_CONN_TS_INTERVAL_NUM);
+            }
+#define CT_RATE(i) \
+    offload_stats.timestamps[(i) & CT_CONN_TS_MASK] == \
+    offload_stats.timestamps[(i - 1) & CT_CONN_TS_MASK] ? 0 : \
+    ((double) CT_CONN_TS_INTERVAL_NUM) / \
+    (offload_stats.timestamps[(i) & CT_CONN_TS_MASK] - \
+     offload_stats.timestamps[(i - 1) & CT_CONN_TS_MASK])
+#define PER_LINE 16
+            for (i = 0; dpctl_p->verbosity && i < CT_CONN_TIMESTAMPS;
+                 i += PER_LINE) {
+                int j;
+
+                ds_put_format(&s, "%4u ", i);
+                for (j = 0; j < PER_LINE; j++) {
+                    ds_put_format(&s, "%4.1f ", CT_RATE(i + j));
+                }
+                ds_put_cstr(&s, "\n");
+            }
             dpctl_print(dpctl_p,
-                        "Offload queue depth: %u (%u - %u)\n"
-                        "Connction tracking offloaded connections: %u\n",
+                        "%sOffload queue depth: %u (%u - %u)\n"
+                        "Connection tracking offloaded connections: %u\n",
+                        ds_cstr(&s),
                         offload_stats.queue_enqueues - offload_stats.queue_dequeues,
                         offload_stats.queue_enqueues, offload_stats.queue_dequeues,
                         offload_stats.ct_connections);
+            ds_destroy(&s);
         } else {
             dpctl_error(dpctl_p, error, "Offloaded statistics could not be retrieved");
         }

lib/dpif.h
@@ -489,10 +489,16 @@ int dpif_port_dump_done(struct dpif_port_dump *);
 int dpif_port_poll(const struct dpif *, char **devnamep);
 void dpif_port_poll_wait(const struct dpif *);
 
+#define CT_CONN_TS_INTERVAL_NUM (1 << 10)
+#define CT_CONN_TS_INTERVAL_MASK (CT_CONN_TS_INTERVAL_NUM - 1)
+#define CT_CONN_TS_BITS 9
+#define CT_CONN_TIMESTAMPS (1 << CT_CONN_TS_BITS)
+#define CT_CONN_TS_MASK (CT_CONN_TIMESTAMPS - 1)
 struct dpif_offload_stats {
     uint32_t ct_connections;
     uint32_t queue_enqueues;
     uint32_t queue_dequeues;
+    long long int timestamps[CT_CONN_TIMESTAMPS];
 };
 
 /* Flow table operations. */

Mellanox/openvswitch

commit 4d1a7cf813ce66d289523695f4ec3711dd701777

dpif-netdev: Fix insertion rate statistics for non-offloaded CT
In case the CT is not offloaded, its "dp" is NULL. Insertion rate
statistics cannot be accessed via NULL pointer. Fix it.

Issue: 2232482
Change-Id: I1df3e752704e46c0896e0274928b687efdd09674
Fixes: 18efc6d35930 ("dpctl: Add CT insertion/deletion rates statistics")
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -2902,7 +2902,7 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
                          dir == CT_DIR_INIT ? "init" : "reply",
                          UUID_ARGS((struct uuid *)&ct_offload[dir].ufid));
             }
-            if (!rets[CT_DIR_INIT] && !rets[CT_DIR_REP]) {
+            if (dp && !rets[CT_DIR_INIT] && !rets[CT_DIR_REP]) {
                 if (ct_offload[CT_DIR_INIT].op ==
                     DP_NETDEV_FLOW_OFFLOAD_OP_ADD) {
                     dp->offload_stats.ct_connections++;
@@ -2910,8 +2910,8 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
                     dp->offload_stats.ct_connections--;
                 }
             }
-            if ((dp->offload_stats.ct_connections &
-                 CT_CONN_TS_INTERVAL_MASK) == 0) {
+            if (dp && (dp->offload_stats.ct_connections &
+                       CT_CONN_TS_INTERVAL_MASK) == 0) {
                 ts_index &= CT_CONN_TS_MASK;
                 dp->offload_stats.timestamps[ts_index++] = time_msec();
             }