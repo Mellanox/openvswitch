Mellanox/OVS

commit a1260dfcb92992c56ac5a67d226f1cf32036b8ff

netdev-offload-dpdk: Don't match zeros on CT properties zero recirc_id
On the first recirc_id, the CT properties are zeroed. Thus, there is no
point on matching those zeros.

Issue: 2110233
Change-Id: Id1f0e2898ec385da8164d57930ce6ef25534eeb1
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/netdev-offload-dpdk.c
@@ -2271,53 +2271,73 @@ parse_flow_match(struct netdev *netdev,
 
     /* ct-state */
     if (match->wc.masks.ct_state &&
-        !(match->wc.masks.ct_state & match->flow.ct_state & CS_NEW) &&
-        !add_pattern_match_reg_field(patterns, REG_FIELD_CT_STATE,
-                                     match->flow.ct_state,
-                                     match->wc.masks.ct_state)) {
-        consumed_masks->ct_state = 0;
+        !(match->wc.masks.ct_state & match->flow.ct_state & CS_NEW)) {
+        if ((!match->flow.recirc_id &&
+             !(match->wc.masks.ct_state & match->flow.ct_state)) ||
+            !add_pattern_match_reg_field(patterns, REG_FIELD_CT_STATE,
+                                         match->flow.ct_state,
+                                         match->wc.masks.ct_state)) {
+            consumed_masks->ct_state = 0;
+        }
     }
     /* ct-zone */
     if (match->wc.masks.ct_zone &&
-        !get_zone_id(match->flow.ct_zone,
-                     &act_resources->ct_match_zone_id) &&
-        !add_pattern_match_reg_field(patterns,
-                                     REG_FIELD_CT_ZONE,
-                                     act_resources->ct_match_zone_id,
-                                     reg_fields[REG_FIELD_CT_ZONE].mask)) {
+        ((!match->flow.recirc_id &&
+          !(match->flow.ct_zone & match->wc.masks.ct_zone)) ||
+         (!get_zone_id(match->flow.ct_zone,
+                       &act_resources->ct_match_zone_id) &&
+          !add_pattern_match_reg_field(patterns,
+                                       REG_FIELD_CT_ZONE,
+                                       act_resources->ct_match_zone_id,
+                                       reg_fields[REG_FIELD_CT_ZONE].mask)))) {
         consumed_masks->ct_zone = 0;
     }
     /* ct-mark */
-    if (match->wc.masks.ct_mark &&
-        !add_pattern_match_reg_field(patterns, REG_FIELD_CT_MARK,
-                                     match->flow.ct_mark,
-                                     match->wc.masks.ct_mark)) {
-        consumed_masks->ct_mark = 0;
+    if (match->wc.masks.ct_mark) {
+        if ((!match->flow.recirc_id &&
+             !(match->flow.ct_mark & match->wc.masks.ct_mark)) ||
+            !add_pattern_match_reg_field(patterns, REG_FIELD_CT_MARK,
+                                         match->flow.ct_mark,
+                                         match->wc.masks.ct_mark)) {
+            consumed_masks->ct_mark = 0;
+        }
     }
     /* ct-label */
-    if (match->wc.masks.ct_label.u32[0] &&
-        !add_pattern_match_reg_field(patterns, REG_FIELD_CT_LABEL0,
-                                     match->flow.ct_label.u32[0],
-                                     match->wc.masks.ct_label.u32[0])) {
-        consumed_masks->ct_label.u32[0] = 0;
+    if (match->wc.masks.ct_label.u32[0]) {
+        if ((!match->flow.recirc_id &&
+             !(match->flow.ct_label.u32[0] & match->wc.masks.ct_label.u32[0])) ||
+            !add_pattern_match_reg_field(patterns, REG_FIELD_CT_LABEL0,
+                                         match->flow.ct_label.u32[0],
+                                         match->wc.masks.ct_label.u32[0])) {
+            consumed_masks->ct_label.u32[0] = 0;
+        }
     }
-    if (match->wc.masks.ct_label.u32[1] &&
-        !add_pattern_match_reg_field(patterns, REG_FIELD_CT_LABEL1,
-                                     match->flow.ct_label.u32[1],
-                                     match->wc.masks.ct_label.u32[1])) {
-        consumed_masks->ct_label.u32[1] = 0;
+    if (match->wc.masks.ct_label.u32[1]) {
+        if ((!match->flow.recirc_id &&
+             !(match->flow.ct_label.u32[1] & match->wc.masks.ct_label.u32[1])) ||
+            !add_pattern_match_reg_field(patterns, REG_FIELD_CT_LABEL1,
+                                         match->flow.ct_label.u32[1],
+                                         match->wc.masks.ct_label.u32[1])) {
+            consumed_masks->ct_label.u32[1] = 0;
+        }
     }
-    if (match->wc.masks.ct_label.u32[2] &&
-        !add_pattern_match_reg_field(patterns, REG_FIELD_CT_LABEL2,
-                                     match->flow.ct_label.u32[2],
-                                     match->wc.masks.ct_label.u32[2])) {
-        consumed_masks->ct_label.u32[2] = 0;
+    if (match->wc.masks.ct_label.u32[2]) {
+        if ((!match->flow.recirc_id &&
+             !(match->flow.ct_label.u32[2] & match->wc.masks.ct_label.u32[2])) ||
+            !add_pattern_match_reg_field(patterns, REG_FIELD_CT_LABEL2,
+                                         match->flow.ct_label.u32[2],
+                                         match->wc.masks.ct_label.u32[2])) {
+            consumed_masks->ct_label.u32[2] = 0;
+        }
     }
-    if (match->wc.masks.ct_label.u32[3] &&
-        !add_pattern_match_reg_field(patterns, REG_FIELD_CT_LABEL3,
-                                     match->flow.ct_label.u32[3],
-                                     match->wc.masks.ct_label.u32[3])) {
-        consumed_masks->ct_label.u32[3] = 0;
+    if (match->wc.masks.ct_label.u32[3]) {
+        if ((!match->flow.recirc_id &&
+             !(match->flow.ct_label.u32[3] & match->wc.masks.ct_label.u32[3])) ||
+            !add_pattern_match_reg_field(patterns, REG_FIELD_CT_LABEL3,
+                                         match->flow.ct_label.u32[3],
+                                         match->wc.masks.ct_label.u32[3])) {
+            consumed_masks->ct_label.u32[3] = 0;
+        }
     }
 
     add_flow_pattern(patterns, RTE_FLOW_ITEM_TYPE_END, NULL, NULL);

Mellanox/openvswitch

commit 0bd3912f01d4cc3bdf3a4c4fe33e3e8af2edf295

netdev-offload-dpdk: Fix multi-zone with zero zone
The cited commit meant to remove the ct-state match of flows with
recirc_id=0, as not needed. It also removed the match of zero CT zone
of such flows. In case of a CT flow, it must have that match, so it's
wrong. There is no match on CT-zone anyway on flows with recirc_id=0, so
reverting this part.

Issue: 2262467
Change-Id: I77f0be1a825fcc1f0231e633dee388f91b45f3f9
Fixes: a1260dfcb929 ("netdev-offload-dpdk: Don't match zeros on CT properties zero recirc_id")
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/netdev-offload-dpdk.c
@@ -2653,14 +2653,12 @@ parse_flow_match(struct netdev *netdev,
     }
     /* ct-zone */
     if (match->wc.masks.ct_zone &&
-        ((!match->flow.recirc_id &&
-          !(match->flow.ct_zone & match->wc.masks.ct_zone)) ||
-         (!get_zone_id(match->flow.ct_zone,
-                       &act_resources->ct_match_zone_id) &&
-          !add_pattern_match_reg_field(patterns,
-                                       REG_FIELD_CT_ZONE,
-                                       act_resources->ct_match_zone_id,
-                                       reg_fields[REG_FIELD_CT_ZONE].mask)))) {
+        (!get_zone_id(match->flow.ct_zone,
+                      &act_resources->ct_match_zone_id) &&
+         !add_pattern_match_reg_field(patterns,
+                                      REG_FIELD_CT_ZONE,
+                                      act_resources->ct_match_zone_id,
+                                      reg_fields[REG_FIELD_CT_ZONE].mask))) {
         consumed_masks->ct_zone = 0;
     }
     /* ct-mark */